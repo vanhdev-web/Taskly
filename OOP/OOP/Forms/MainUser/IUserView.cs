e9e17b3415f7a19e7e21fcdb8bc3fa20e04ac129
Kanjiforwork <153203191+Kanjiforwork@users.noreply.github.com>
2025-05-02 12:40:00 +0700

    Revert "fix folder"
    This reverts commit 0592cc7372060274364432ce50304ddbb65f3f1d.

---
+++ OOP/OOP/Forms/MainUser/IUserView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Forms.MainUser
+{
+    public interface IUserView
+    {
+        int CurrentUserID { get; }
+        void DisplayActivityHistory(string[] activities);
+        void DisplayUser(string username, string email, byte[] avatar);
+        void DisplayProjects(string[] projectNames);
+        string OldPassword { get; }
+        string NewPassword { get; }
+        string ConfirmPassword { get; }
+        void ShowMessage(string message);
+    }
+}
